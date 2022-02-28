e381f15ed996197bd3ea4ea4476c96500d55ad6b
David Kai Landes <davidlandes24@gmail.com>
2022-02-27 21:15:01 -0500

    Expence Tracker Project Done

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -4,7 +4,7 @@ import AppReducer from "./AppReducer";
 // Initial State
 const initialState = {
   transactions: [
-    { id: 1, text: "Fuguitos", amount: -20 },
+    { id: 1, text: "Jugos", amount: -8.99 },
     { id: 2, text: "Salary", amount: 500 },
     { id: 3, text: "Books", amount: -14.99 },
     { id: 4, text: "Camara", amount: -150 },
@@ -18,10 +18,27 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
+  // Actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  }
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  }
+
   return (
     <GlobalContext.Provider
       value={{
         transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
       }}
     >
       {children}
